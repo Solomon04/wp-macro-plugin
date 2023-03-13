9d0ff6af574d66e4bf3215e14a3e3094e98a888d
Solomon04 <antoinesolomon5@gmail.com>
2023-03-13 18:33:46 -0500

    add asana

---
+++ asana-template.md
@@ -0,0 +1,65 @@
+### Name
+
+{full_name}
+
+### Email
+
+{email}
+
+### What is your experience with training and nutrition?
+
+{training_experience}
+
+
+
+### What is your experience with training and nutrition?
+
+{macro_experience}
+
+
+
+### Unit
+
+{unit}
+
+
+
+### Weight
+
+{weight}
+
+
+
+### Height
+
+{height}
+
+
+
+### What are your day-to-day activity levels?
+
+{activity_level}
+
+
+
+### What are you looking to accomplish?
+
+{goal}
+
+
+
+### What can we help you with?
+
+{call_to_action}
+
+
+
+### Are you interested in a one on one free initial health assessment with a Registered Dietitian that specializes in weight loss?
+
+{wants_consulting}
+
+
+
+* * *
+
+_This task was submitted through the macro calculator via WordPress_
--- macroapi.php
+++ macroapi.php
@@ -3,7 +3,7 @@
 Plugin Name: Macrocalculator API
 Plugin URI: https://github.com/Solomon04/react-macro
 Description: This is a custom plugin that allows us to email users that complete the macro calculator form.
-Version: 1.1
+Version: 1.2
 Author: Solomon <solomon@icodestuff.io>
 */
 
@@ -17,6 +17,7 @@ $dotenv->load();
 use Rakit\Validation\Validator;
 use SendGrid\Mail\Mail;
 use GuzzleHttp\Client;
+use Asana\Client as Asana;
 
 
 // Your plugin code goes here
@@ -35,6 +36,14 @@ function handle($request)
         'email' => 'required|email',
         'tdee' => 'required|numeric',
         'diets' => 'required',
+        'training_experience' => 'required',
+        'macro_experience' => 'required',
+        'unit' => 'required',
+        'weight' => 'required|numeric',
+        'activity_level' => 'required',
+        'goal' => 'required',
+        'call_to_action' => 'required',
+        'wants_consulting' => 'required'
     ]);
 
     $validation->validate();
@@ -63,6 +72,12 @@ function handle($request)
         send_sendgrid_email($email, $firstName, $tdee, $diets);
     } catch (Exception $exception) {
         return new WP_Error(400, $exception->getMessage());
+    }
+
+    try {
+        add_submission_to_asana($params);
+    }catch (\Asana\Errors\AsanaError $exception) {
+        return new WP_Error($exception->getCode(), $exception->getMessage());
     }
 
     return new WP_REST_Response(
@@ -118,4 +133,47 @@ function add_subscriber_to_convert_kit($email, $fullName, $firstName)
         'status' => $response->getStatusCode(),
         'message' => $response->getBody()->getContents()
     ];
+}
+
+function add_submission_to_asana($params)
+{
+    $asana = Asana::accessToken(getenv('ASANA_PERSONAL_ACCESS_TOKEN'));
+
+    $template = file_get_contents(__DIR__ . '/asana-template.html');
+    $weight = sprintf("%d%s", $params['weight'], $params['unit'] === 'Metric' ? 'kg' : 'lbs');
+    $height = sprintf("%d%s", $params['height'], $params['unit'] === 'Metric' ? 'cm' : 'in');
+    $body = str_replace([
+        '{full_name}',
+        '{email}',
+        '{training_experience}',
+        '{macro_experience}',
+        '{unit}',
+        '{weight}',
+        '{height}',
+        '{activity_level}',
+        '{goal}',
+        '{call_to_action}',
+        '{wants_consulting}',
+    ], [
+        $params['full_name'],
+        $params['email'],
+        $params['training_experience'],
+        $params['macro_experience'],
+        $params['unit'],
+        $weight,
+        $height,
+        $params['activity_level'],
+        $params['goal'],
+        $params['call_to_action'],
+        $params['wants_consulting'],
+    ], $template);
+
+    $data = [
+        'name' => "{$params['full_name']} macro submission - " . date("Y-m-d"),
+        'projects' => ['1204051454931105'],
+        'workspace' => '1201738035299370',
+        'html_notes' => $body
+    ];
+
+    return $asana->tasks->createTask($data, ['opt_pretty' => true, 'opt_fields' => ['custom_fields', 'html_notes', 'name', 'workspace', 'projects']]);
 }
